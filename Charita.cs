ca27c7b1c0e202b4047b6ca84cbe1f2bf0ef12ec
MilanBorovy <m.borovy98@gmail.com>
2018-01-31 23:09:39 +0100

    Removed old files

--- Charita.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class Charita {
-
-    public static string[] ch = new string[9] { "Nic", "Útulek", "Hřiště", "Knihovna", "MŠ", "Hospic", "Farmaceutický výzkum", "Humanitární pomoc", "Kosmický výzkum" };
-
-}
